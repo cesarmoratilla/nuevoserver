94a509f6429a91726a728e8e1a19c9485dbd061e
cesarmoratilla <68732275+cesarmoratilla@users.noreply.github.com>
2020-07-26 15:37:38 -0500

    resources

---
+++ resources/[local]/[esx]/esx_adminplus/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+Config.Locale = 'es'
+Config.reportCooldown = 60 --seconds
+Config.warnMax = 3  --how many warn player can get before getting kicked?
+Config.adminRanks = { -- change this as your server ranking ( default are : superadmin | admin | moderator )
+				'admin',
+				'moderator',
+				--'jradmin',
+				--'sradmin',
+				--'headadmin',
+				--'moderator',
+				--'manager',
+				--'owner',
+				--'developer',
+            }
